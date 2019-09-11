931068234c6024f10379430257fdc6eb14c050f2
barrymun <barrymun@tcd.ie>
2019-09-11 18:29:39 +0200

    29

---
+++ 20-29/29.py
@@ -0,0 +1,27 @@
+MIN = 2
+MAX = 100
+
+def power_of(n, exp):
+    r = 1
+    i = 0
+    while i < exp:
+        r *= n
+        i += 1
+    return r
+
+def get_combinations(n):
+    r = []
+    for i in range(MIN, MAX + 1):
+        x = power_of(n, i)
+        r.append(x)
+    return r
+
+
+
+if __name__ == "__main__":
+    r = []
+    for i in range(MIN, MAX + 1):
+        x = get_combinations(i)
+        r = r + x
+    r = list(set(r))
+    print len(r)
