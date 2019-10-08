93cd5ed825cc01856bb1c809e98155599799eb4a
barrymun <barrymun@tcd.ie>
2019-10-08 16:11:09 +0200

    completed 32

--- 30-39/32.py
+++ 30-39/32.py
@@ -1,3 +1,5 @@
+import time
+
 MAX_LEN = 9
 DIGIT_CHECK = [str(i) for i in xrange(1, 10)]
 
@@ -5,7 +7,11 @@ def is_pandigital(n):
     """
     """
     if len(n) > MAX_LEN or len(n) < MAX_LEN:
+        # sanity check
         return False
+
+    # can do this but seems to be slower
+    # return set(DIGIT_CHECK) == set(n)
 
     check = True
     for digit in DIGIT_CHECK:
@@ -17,25 +23,34 @@ def is_pandigital(n):
     return check
 
 if __name__ == "__main__":
+    start = time.time()
     r = []
+
+    # single digit
+    for i in xrange(1, 10):
+        for j in xrange(111, 10000):
+            p = i * j
+            n = "{}{}{}".format(i, j, p)
+            if (len(n) > MAX_LEN):
+                # all numbers beyond this point will be greater, so better to exit the loop to improve efficiency
+                break
+            if not is_pandigital(n=n):
+                continue
+            r.append(p)
+
     for i in xrange(11, 100):
         for j in xrange(111, 1000):
             p = i * j
-
-            # if (len("{}{}{}".format(i, j, p)) > MAX_LEN or len("{}{}{}".format(i, j, p)) < MAX_LEN):
-            #     # all numbers beyond this point will be greater, so better to exit the loop
-            #     break
-
-            if not is_pandigital(n="{}{}{}".format(i, j, p)):
+            n = "{}{}{}".format(i, j, p)
+            if (len(n) > MAX_LEN or len(n) < MAX_LEN):
+                # all numbers beyond this point will be greater, so better to exit the loop to improve efficiency
+                break
+            if not is_pandigital(n=n):
                 continue
-
             r.append(p)
 
-    print ""
-    print len(r)
-    r = list(set(r))
-    print len(r)
+    r = list(set(r))  # removing duplicates
+    print sum(int(n) for n in r)  # answer
 
-    print ""
-    print r
-    print sum(int(n) for n in r)
+    end = time.time()
+    print end - start
