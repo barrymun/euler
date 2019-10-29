ba4e0df1eb50ddbbb0372337c6e6307b716d0598
barrymun <barrymun@tcd.ie>
2019-10-29 12:51:45 +0100

    44, but upper bound guessed by trial and error

--- 40-49/44.py
+++ 40-49/44.py
@@ -1,7 +1,5 @@
 import sys
 
-tracker = {}
-
 def get_pentagonal_number(n):
     """
     """
@@ -10,38 +8,29 @@ def get_pentagonal_number(n):
 def generate_pentagonal_numbers(upper_limit):
     """
     """
-    i = 1  # index
-    pn = 0  # pentagonal number
-    while pn < upper_limit:
+    p_numbers = []
+    for i in xrange(1, upper_limit):
         pn = get_pentagonal_number(n=i)
-        if pn not in tracker:
-            tracker[pn] = True
-        i += 1
+        p_numbers.append(pn)
+    return p_numbers
 
 if __name__ == "__main__":
-    r = 0
+    l = 3000
 
-    for k in xrange(1, 500):
+    r = 0
+    pn = generate_pentagonal_numbers(upper_limit=l)
+
+    for j in xrange(1, l):
         if r != 0:
             break
 
-        for j in xrange(1, 500):
-
-            pk = get_pentagonal_number(n=k)
+        for k in xrange(j, l):
             pj = get_pentagonal_number(n=j)
+            pk = get_pentagonal_number(n=k)
+            s = pj + pk
+            d = abs(pj - pk)
 
-            s = pk + pj
-            d = pk - pj
-            d = abs(d)
-            # print pk, pj, s, d
-
-
-            gen_pn = generate_pentagonal_numbers(upper_limit=s)
-            # print tracker
-            # sys.exit()
-            if s in tracker and d in tracker:
-                print pk, pj, s, d
-                print tracker[s], tracker[d]
+            if s in pn and d in pn:
                 r = d
                 break
 
